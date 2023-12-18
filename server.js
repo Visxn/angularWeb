2aadb607b7ce0c3e60a27cfe41e1b6a5bb948234
Roger Vison <56312019+Visxn@users.noreply.github.com>
2023-12-18 10:40:55 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
+
 const app = express();
+app.use(cors({ origin: 'http://localhost:4200' }));
+
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:4200", 
+    methods: ["GET", "POST"]
+  }
+});
 
 io.on('connection', (socket) => {
-  console.log('Usuario conectado');
+  console.log('Nuevo cliente conectado');
 
-  socket.on('mensaje', (mensaje) => {
-    console.log('Mensaje recibido:', mensaje);
-    // Emitir el mensaje a todos los clientes conectados, incluido el emisor
-    io.emit('mensaje', mensaje);
+  socket.on('message', (message) => {
+    console.log(message);
+    socket.broadcast.emit('message', message);
   });
+
 
   socket.on('disconnect', () => {
-    console.log('Usuario desconectado');
+    console.log('Cliente desconectado');
   });
 });
 
+setInterval(() => {
+  const randomNumber = Math.floor(Math.random() * 10); // Genera un número aleatorio entre 0 y 9
+  io.emit('number', randomNumber);
+  console.log('Enviando número aleatorio: ', randomNumber);
+},1000);
 
-const path = __dirname + '/dist/my-app';
-app.use(express.static(path));
 
-app.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`));
